11ed42e1f8c9d85175b1a993bdf0c4762a8fa835
Franciswyyy <826531768@qq.com>
2018-09-04 20:48:32 +0800

    redis Test

--- src/main/java/com/imooc/miaosha/controller/DemoController.java
+++ src/main/java/com/imooc/miaosha/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.imooc.miaosha.controller;
 
 import com.imooc.miaosha.domain.User;
+import com.imooc.miaosha.redis.RedisService;
+import com.imooc.miaosha.redis.UserKey;
 import com.imooc.miaosha.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,6 +20,8 @@ public class DemoController {
 	@Autowired
 	UserService userService;
 
+	@Autowired
+	RedisService redisService;
 
 	@RequestMapping("/")
 	@ResponseBody
@@ -60,5 +64,22 @@ public class DemoController {
 //		userService.tx();
 //		return Result.success(true);
 //	}
+
+	@RequestMapping("/redis/get")
+	@ResponseBody
+	public Result<User> redisGet() {
+		User  user  = redisService.get(UserKey.getById, ""+1, User.class);
+		return Result.success(user);
+	}
+
+	@RequestMapping("/redis/set")
+	@ResponseBody
+	public Result<Boolean> redisSet() {
+		User user  = new User();
+		user.setId(1);
+		user.setName("1111");
+		redisService.set(UserKey.getById, ""+1, user);   //UserKey:id1
+		return Result.success(true);
+	}
 
 }
